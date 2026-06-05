65b630ce58644e3231214f2629d2c4e1d6df535a
Mazen Eldesouky <maznaldswqy96@gmail.com>
2026-06-05 05:27:56 +0300

    Create onboardingData.tsx

---
+++ src/constants/onboardingData.tsx
@@ -0,0 +1,11 @@
+export const ONBOARDING_DATA = [
+  {
+    id: 1,
+    title: "Welcome to MindEcho",
+    description: "Your journey towards mental well-being starts here, guided by elite specialists.",
+    image: "/Photos/logo.png", // استخدم اللوجو أو صورة تعبيرية بجودة عالية
+    color: "#2563eb",
+    bgGradient: "from-blue-50 to-white"
+  },
+  
+];
