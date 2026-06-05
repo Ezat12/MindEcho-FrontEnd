f13d1620e0a3b474d6fbc99444e0637e2a9c2d23
Mazen Eldesouky <maznaldswqy96@gmail.com>
2026-06-05 05:28:35 +0300

    Create OnboardingScreen component for onboarding flow

---
+++ src/constants/OnboardingScreen.tsx
@@ -0,0 +1,14 @@
+import { ONBOARDING_DATA } from '../constants/onboardingData';
+
+const OnboardingScreen = () => {
+  const [currentStep, setCurrentStep] = useState(0);
+  const data = ONBOARDING_DATA[currentStep];
+
+  return (
+    <div>
+      <img src={data.image} alt={data.title} />
+      <h1>{data.title}</h1>
+      <p>{data.description}</p>
+    </div>
+  );
+};
